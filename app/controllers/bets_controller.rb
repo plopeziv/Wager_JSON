3dfdc6739c70a83135bd173a2a6112e2e9ba56dc
plopeziv <plopez@8thlight.com>
2022-03-28 16:43:00 -0500

    Configure routes for tables

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,2 +1,6 @@
 class BetsController < ApplicationController
+    def index
+        @bets = Bet.all
+        render json: @bets
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :bets
-  resources :users
+  resources :bets, only: [:index]
+  resources :users, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
