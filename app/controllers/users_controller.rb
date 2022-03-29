278763bf2a4db9ffead708e119980878d086d099
plopeziv <plopez@8thlight.com>
2022-03-28 21:58:26 -0500

    Allow creation of User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
         @users = User.all
         render json: @users
     end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def create
+        @user = User.create(
+            userId: params[:userId],
+            name: params[:name],
+            accountBalance: params[:accountBalance]
+        )
+        render json: @user
+    end
 end
